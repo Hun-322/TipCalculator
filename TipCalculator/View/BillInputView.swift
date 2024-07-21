0ba223559108513e30fa17c57959857db22b16ea
Hun <ksh26271@naver.com>
2024-07-22 02:36:46 +0900

    [feat] UI 기본 뼈대 구현

---
+++ TipCalculator/View/BillInputView.swift
@@ -0,0 +1,24 @@
+//
+//  BillInputView.swift
+//  TipCalculator
+//
+//  Created by KSH on 7/22/24.
+//
+
+import UIKit
+
+class BillInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .blue
+    }
+}
---
+++ TipCalculator/View/TipInputView.swift
@@ -0,0 +1,24 @@
+//
+//  TipInputView.swift
+//  TipCalculator
+//
+//  Created by KSH on 7/22/24.
+//
+
+import UIKit
+
+class TipInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .systemPink
+    }
+}
