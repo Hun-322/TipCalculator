f57046004ba161b8e9c549871bc4f719a287cd10
Hun <ksh26271@naver.com>
2024-07-22 12:55:58 +0900

    [feat] UIColor hexString을 color로 바꿔주는 로직 extension 추가, ThemeColor 선언

---
+++ TipCalculator/Config/ThemeFont.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeFont.swift
+//  TipCalculator
+//
+//  Created by KSH on 7/22/24.
+//
+
+import Foundation
---
+++ TipCalculator/Config/themeColor.swift
@@ -0,0 +1,16 @@
+//
+//  themeColor.swift
+//  TipCalculator
+//
+//  Created by KSH on 7/22/24.
+//
+
+import UIKit
+
+struct ThemeColor {
+    static let bg = UIColor(hexString: "F5F3F4")
+    static let primary = UIColor(hexString: "1CC9BE")
+    static let secondary = UIColor.systemOrange
+    static let text = UIColor(hexString: "000000")
+    static let separator = UIColor(hexString: "CCCCCC")
+}
