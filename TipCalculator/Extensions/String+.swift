98eee2d658d5c004868966f600766b69dd2fe99c
Hun <ksh26271@naver.com>
2024-07-24 15:57:45 +0900

    [feat] Input 유효성 검증용 doubleValue 추가

---
+++ TipCalculator/Extensions/String+.swift
@@ -0,0 +1,14 @@
+//
+//  String+.swift
+//  TipCalculator
+//
+//  Created by KSH on 7/24/24.
+//
+
+import Foundation
+
+extension String {
+    var doubleValue: Double? {
+        Double(self)
+    }
+}
