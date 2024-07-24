8e09eb5b8f3ebbdb9213e6c267c2ab48b4ed2ad4
Hun <ksh26271@naver.com>
2024-07-25 00:01:29 +0900

    [feat] CustomTipButton 클릭 시 Alert으로 팁 퍼센트 정하도록 구현

--- TipCalculator/View/TipInputView.swift
+++ TipCalculator/View/TipInputView.swift
@@ -53,6 +53,9 @@ class TipInputView: UIView {
         button.titleLabel?.font = ThemeFont.bold(ofSize: 20)
         button.backgroundColor = ThemeColor.primary
         button.addCornerRadius(radius: 8.0)
+        button.tapPublisher.sink { [weak self] _ in
+            self?.handleCustomTipButton()
+        }.store(in: &cancellables)
         return button
     }()
     
@@ -108,6 +111,35 @@ class TipInputView: UIView {
             make.width.equalTo(68)
             make.centerY.equalTo(buttonHSatckView.snp.centerY)
         }
+    }
+    
+    private func handleCustomTipButton() {
+        let alertController: UIAlertController = {
+            let controller = UIAlertController(
+                title: "Enter custom tip",
+                message: nil,
+                preferredStyle: .alert
+            )
+            controller.addTextField { textField in
+                textField.placeholder = "Make it generous !"
+                textField.keyboardType = .numberPad
+                textField.autocorrectionType = .no
+            }
+            let cancelAction = UIAlertAction(
+                title: "Cancel",
+                style: .cancel
+            )
+            let okAction = UIAlertAction(
+                title: "OK",
+                style: .default) { [weak self] _ in
+                    guard let text = controller.textFields?.first?.text,
+                          let value = Int(text) else { return }
+                    self?.tipSubject.send(.custom(value: value))
+                }
+            [okAction, cancelAction].forEach(controller.addAction(_:))
+            return controller
+        }()
+        parentViewController?.present(alertController, animated: true)
     }
     
     private func buildTipButton(tip: Tip) -> UIButton {
