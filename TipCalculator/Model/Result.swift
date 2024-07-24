febe791fd8fd577aebb31f4bb8e2d4494faa4b4a
Hun <ksh26271@naver.com>
2024-07-24 15:37:56 +0900

    [feat] Result Model 정의

---
+++ TipCalculator/Model/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  TipCalculator
+//
+//  Created by KSH on 7/24/24.
+//
+
+import Foundation
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTip: Double
+}
