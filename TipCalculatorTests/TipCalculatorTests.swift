bb729afd1f1f3e1253695baeff6a4b166f6f54c2
Hun <ksh26271@naver.com>
2024-07-29 22:12:03 +0900

    [feat] 팁 주는 방법 여러 상황 추가

--- TipCalculatorTests/TipCalculatorTests.swift
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -32,7 +32,7 @@ final class TipCalculatorTests: XCTestCase {
     //    - 1000원
     //    - 팁 없음
     //    - 1명
-    func test_한명이팁을안주는경우() {
+    func test_1명이팁을안주는경우() {
         // given: 필요한 value 세팅
         let bill: Double = 1000.0
         let tip: Tip = .none
@@ -49,6 +49,66 @@ final class TipCalculatorTests: XCTestCase {
             XCTAssertEqual(result.amountPerPerson, 1000.0)
             XCTAssertEqual(result.totalBill, 1000)
             XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func test_2명이팁을안주는경우() {
+        // given: 필요한 value 세팅
+        let bill: Double = 1000.0
+        let tip: Tip = .none
+        let split: Int = 2
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split
+        )
+        // when: 테스트 코드 실행
+        let output = sut.transform(input: input)
+        // then: 결과 확인(출력)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 500.0)
+            XCTAssertEqual(result.totalBill, 1000)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func test_2명이팁을10퍼센트주는경우() {
+        // given: 필요한 value 세팅
+        let bill: Double = 1000.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split
+        )
+        // when: 테스트 코드 실행
+        let output = sut.transform(input: input)
+        // then: 결과 확인(출력)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 550.0)
+            XCTAssertEqual(result.totalBill, 1100)
+            XCTAssertEqual(result.totalTip, 100)
+        }.store(in: &cancellables)
+    }
+    
+    func test_4명이커스텀팁을주는경우() {
+        // given: 필요한 value 세팅
+        let bill: Double = 2000.0
+        let tip: Tip = .custom(value: 201)
+        let split: Int = 4
+        let input = buildInput(
+            bill: bill,
+            tip: tip,
+            split: split
+        )
+        // when: 테스트 코드 실행
+        let output = sut.transform(input: input)
+        // then: 결과 확인(출력)
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 550.25)
+            XCTAssertEqual(result.totalBill, 2201)
+            XCTAssertEqual(result.totalTip, 201)
         }.store(in: &cancellables)
     }
     
