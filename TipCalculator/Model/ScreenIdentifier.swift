436abc262652b5dc2133bc33aabedd9f3d09fa5e
Hun <ksh26271@naver.com>
2024-07-31 13:18:06 +0900

    [feat] AccessibilityIdentifier NameSapce 선언

---
+++ TipCalculator/Model/ScreenIdentifier.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenIdentifier.swift
+//  TipCalculator
+//
+//  Created by KSH on 7/31/24.
+//
+
+import Foundation
+
+enum ScreenIdentifier {
+    
+    enum ResultView: String {
+        case totalAmountPerPersonValueLabel
+        case totalBillValueLabel
+        case totalTipValueLabel
+    }
+}
---
+++ TipCalculatorUITests/CalculatorScreen.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorScreen.swift
+//  TipCalculatorUITests
+//
+//  Created by KSH on 7/31/24.
+//
+
+import Foundation
